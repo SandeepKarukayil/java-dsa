4e83f7b495266aee7776618370b6676909423969
SandeepKarukayil <115607146+SandeepKarukayil@users.noreply.github.com>
2024-02-25 13:47:06 +0530

    print

---
+++ 01Basics/printName.java
@@ -0,0 +1,11 @@
+public class printName{
+
+    public static void main(String[] args) {
+        // System.out.println("Sandeep");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
